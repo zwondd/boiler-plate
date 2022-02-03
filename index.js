129bde2fefc3c9beebc55af640b77daca1a71022
zwondd <zwondd@gmail.com>
2022-02-03 22:50:51 +0900

    Add register post method

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 
@@ -11,5 +16,20 @@ mongoose.connect('mongodb+srv://jiwonjeoung:wldnjs9960@boilerplate.l7ahe.mongodb
 .catch(err => console.log( err) )
 
 app.get('/', (req,res) =>  res.send('Hello World!') )
+
+app.post('/register', (req,res) =>  {
+    // 회원가입 할 때 필요한 정보들을 client 에서 가져오면 
+    // 그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+    user.save((err, userInfo)=>{
+        if( err ) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+
 
 app.listen(port, ()=> console.log(`Example app listening to port ${port}!`))
